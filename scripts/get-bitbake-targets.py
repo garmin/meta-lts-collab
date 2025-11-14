c835ad1928cc0285a3726678a0d73c37c383cc5c
Colin Pinnell McAllister <colinmca242@gmail.com>
2025-11-14 11:39:10 -0600

    ci: Add targeted builds
    Updates main.yml jobs to only build recipes affected by a PR. This more
    targeted approach should reduce build times.
    
    Signed-off-by: Colin Pinnell McAllister <colinmca242@gmail.com>

--- scripts/get-bitbake-targets.py
+++ scripts/get-bitbake-targets.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+from itertools import chain
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 
 def get_bpn(pn_path: Path) -> str:
@@ -15,21 +17,80 @@ def get_bpn(pn_path: Path) -> str:
     return pn_path.stem.split("_")[0]
 
 
-def get_all_bb_files(base_path: Path) -> List[Path]:
-    """Returns a list of all .bb and .bbappend files within a given path
+def get_associated_bpns(modified_file: Path) -> List[str]:
+    """Gets all BPNs that are likley assoicated with a modified file
 
     Args:
-        base_path (Path): Base directory to search from
+        modified_file (Path): Path to modified file
 
     Returns:
-        List[Path]:
+        List[str]: One more BPNs that are assoicated with the modified file
     """
-    return list(base_path.rglob("*.bb*"))
+    search_dir = (
+        modified_file.parent
+        if modified_file.suffix in (".bb", ".bbappend", ".inc")
+        else modified_file.parent.parent
+    )
+    return list(map(get_bpn, search_dir.glob("*.bb*")))
+
+
+def get_modified_bpns(modified_files: List[Path]) -> List[str]:
+    """Gets all BPNs that are assoicated with a list of modified files.
+
+    Args:
+        modified_files (List[Path]): List of files that have been modified.
+
+    Returns:
+        List[str]: All BPNs that are associated with the modified file.
+    """
+    # We should be safe and build everything if a .conf or .bbclass file is modified
+    if any(
+        filter(
+            lambda x: x.suffix in (".conf", ".bbclass"),
+            modified_files,
+        )
+    ):
+        return get_all_bpns()
+    # Otherwise get all possibly modified recipe BPNs
+    return sorted(list(set(chain(*map(get_associated_bpns, modified_files)))))
+
+
+def get_all_bpns() -> List[str]:
+    """Returns all BPNs found in the layer.
+
+    Returns:
+        List[str]: List of all BPNs found in the layer.
+    """
+    layer_dir = Path(__file__).resolve().parent.parent
+    return sorted(list(map(get_bpn, layer_dir.rglob("*.bb*"))))
+
+
+def get_bpns(modified_files: Optional[List[Path]] = None) -> List[str]:
+    """Either gets a list of all the BPNs in the layer, or just the BPNs that are associated with modified files.
+
+    Args:
+        modified_files (Optional[List[Path]], optional): Files that have been modified. Defaults to None.
+
+    Returns:
+        List[str]: All BPNs in the layer, or all associated BPNs if modified files are provided
+    """
+    return get_modified_bpns(modified_files) if modified_files else get_all_bpns()
 
 
 def main() -> None:
-    layer_dir = Path(__file__).resolve().parent.parent
-    print(" ".join(sorted(map(get_bpn, get_all_bb_files(layer_dir)))))
+    parser = ArgumentParser("Gets bitbake targets to build")
+    parser.add_argument(
+        "changed_files",
+        nargs="*",
+        type=Path,
+        help="Files that were modified. "
+        "If files are included in the command, only modified BPNs will be returned. "
+        "If no files are supplied, all BPNs in the layer will be returned.",
+    )
+
+    args = parser.parse_args()
+
+    print(" ".join(get_bpns(getattr(args, "changed_files", None))))
 
 
 if __name__ == "__main__":
