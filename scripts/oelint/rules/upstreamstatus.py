7fb4fb3229f2afc8383f011cbf509ace58007634
Colin Pinnell McAllister <colinmca242@gmail.com>
2026-07-27 12:50:17 +0000

    scripts: fix ruff errors
    Fixes several issues identified by the latest version of ruff (format
    and check).
    
    Signed-off-by: Colin Pinnell McAllister <colinmca242@gmail.com>
    (cherry picked from commit b626cb5c1ae64cc7a8b0665b036c0447996b5ad5)
    Signed-off-by: Colin Pinnell McAllister <colinmca242@gmail.com>

--- scripts/oelint/rules/upstreamstatus.py
+++ scripts/oelint/rules/upstreamstatus.py
@@ -1,40 +1,43 @@
 import os
-from typing import List, Tuple
 
 import regex
+from oelint_adv.cls_rule import Classification, Rule
 from oelint_parser.cls_item import Variable
 from oelint_parser.cls_stash import Stash
 from oelint_parser.rpl_regex import RegexRpl
 
-from oelint_adv.cls_rule import Rule, Classification
-
 
 class LTSCollabRulePatchUpstreamStatus(Rule):
     def __init__(self) -> None:
-        super().__init__(id='oelint.ltscollab.upstreamstatus',
-                         severity='warning',
-                         run_on=[Classification.BBAPPEND,
-                                 Classification.RECIPE],
-                         message="Patch '{FILE}' should contain an Upstream-Status entry")
+        super().__init__(
+            id="oelint.ltscollab.upstreamstatus",
+            severity="warning",
+            run_on=[Classification.BBAPPEND, Classification.RECIPE],
+            message="Patch '{FILE}' should contain an Upstream-Status entry",
+        )
 
-    def _get_recipe(self, items: List[Variable], path: str) -> List[Variable]:
+    def _get_recipe(self, items: list[Variable], path: str) -> list[Variable]:
         # Find matching SRC_URI assignment
         return [x for x in items if x.VarValue.find(os.path.basename(path)) != -1]
 
-    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+    def check(self, _file: str, stash: Stash) -> list[tuple[str, int, str]]:
         res = []
-        patches = stash.GetFiles(_file, '*.patch')
-        patches.extend(stash.GetFiles(_file, '*.diff'))
-        items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
-                                   attribute=Variable.ATTR_VAR, attributeValue='SRC_URI')
+        patches = stash.GetFiles(_file, "*.patch")
+        patches.extend(stash.GetFiles(_file, "*.diff"))
+        items = stash.GetItemsFor(
+            filename=_file,
+            classifier=Variable.CLASSIFIER,
+            attribute=Variable.ATTR_VAR,
+            attributeValue="SRC_URI",
+        )
 
         valid_class = {
-            'Backport': r'Backport',
-            'Inactive-Upstream': r'Inactive-Upstream(\s+\[.*\])*',
-            'Submitted': r'Submitted(\s+\[.*\])*',
+            "Backport": r"Backport",
+            "Inactive-Upstream": r"Inactive-Upstream(\s+\[.*\])*",
+            "Submitted": r"Submitted(\s+\[.*\])*",
         }
-        if _file.endswith('.bb'):
-            valid_class['Inappropriate'] = r'Inappropriate(\s+\[.*\])*'
+        if _file.endswith(".bb"):
+            valid_class["Inappropriate"] = r"Inappropriate(\s+\[.*\])*"
 
         for i in patches:
             with open(i) as input_:
@@ -46,19 +49,31 @@ class LTSCollabRulePatchUpstreamStatus(Rule):
                     continue  # pragma: no cover
                 try:
                     cnt = input_.read()
-                    for m in RegexRpl.finditer(r'^Upstream-Status:\s*(?P<class>.*)', cnt, flags=regex.regex.MULTILINE):
+                    for m in RegexRpl.finditer(
+                        r"^Upstream-Status:\s*(?P<class>.*)",
+                        cnt,
+                        flags=regex.regex.MULTILINE,
+                    ):
                         found = True
-                        if not any(RegexRpl.match(v, m.group('class')) for k, v in valid_class.items()):
+                        if not any(
+                            RegexRpl.match(v, m.group("class"))
+                            for k, v in valid_class.items()
+                        ):
                             _msg = "Upstream-Status in '{FILE}' doesn't pick from valid classifiers {cls}".format(
-                                FILE=os.path.basename(i), cls=','.join(sorted(valid_class.keys())),
+                                FILE=os.path.basename(i),
+                                cls=",".join(sorted(valid_class.keys())),
                             )
-                            res += self.finding(recipe_match.Origin,
-                                                recipe_match.InFileLine,
-                                                override_msg=_msg)
+                            res += self.finding(
+                                recipe_match.Origin,
+                                recipe_match.InFileLine,
+                                override_msg=_msg,
+                            )
                     if not found:
-                        res += self.finding(recipe_match.Origin,
-                                            recipe_match.InFileLine,
-                                            self.Msg.replace('{FILE}', os.path.basename(i)))
+                        res += self.finding(
+                            recipe_match.Origin,
+                            recipe_match.InFileLine,
+                            self.Msg.replace("{FILE}", os.path.basename(i)),
+                        )
                 except UnicodeDecodeError:  # pragma: no cover
                     pass  # pragma: no cover
         return res
