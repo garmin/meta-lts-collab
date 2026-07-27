b626cb5c1ae64cc7a8b0665b036c0447996b5ad5
Colin Pinnell McAllister <colinmca242@gmail.com>
2026-07-27 12:50:17 +0000

    scripts: fix ruff errors
    Fixes several issues identified by the latest version of ruff (format
    and check).
    
    Signed-off-by: Colin Pinnell McAllister <colinmca242@gmail.com>

--- scripts/oelint/rules/backportsonly.py
+++ scripts/oelint/rules/backportsonly.py
@@ -1,17 +1,19 @@
-from typing import List, Tuple
-
+from oelint_adv.cls_rule import Classification, Rule
 from oelint_parser.cls_stash import Stash
-
-from oelint_adv.cls_rule import Rule, Classification
 
 
 class LTSCollabRuleBackportsOnly(Rule):
     def __init__(self) -> None:
-        super().__init__(id='oelint.ltscollab.backportsonly',
-                         severity='error',
-                         run_on=[
-                             Classification.BBCLASS, Classification.DISTROCONF, Classification.MACHINECONF],
-                         message="meta-lts-collab should not contain any distro, machine configs or bbclasses")
+        super().__init__(
+            id="oelint.ltscollab.backportsonly",
+            severity="error",
+            run_on=[
+                Classification.BBCLASS,
+                Classification.DISTROCONF,
+                Classification.MACHINECONF,
+            ],
+            message="meta-lts-collab should not contain any distro, machine configs or bbclasses",
+        )
 
-    def check(self, _file: str, stash: Stash) -> List[Tuple[str, int, str]]:
+    def check(self, _file: str, stash: Stash) -> list[tuple[str, int, str]]:
         return self.finding(_file, 1)
